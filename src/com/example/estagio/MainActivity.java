8adf00ca9f2e5b91da02ecb70d9cb63780278495
Keylla <keylla03@gmail.com>
2014-10-12 16:56:42 -0300

    Retirada de test

--- src/com/example/estagio/MainActivity.java
+++ src/com/example/estagio/MainActivity.java
@@ -46,8 +46,7 @@ public class MainActivity extends ActionBarActivity {
 	    	
         }	
         else{
-        	UsuarioConnect uc = new UsuarioConnect();
-        	System.out.println(uc.busca("admin"));
+        	
 	        Toast.makeText(getApplicationContext(), "Credenciais erradas!",Toast.LENGTH_SHORT).show();
 	        counter--;
 	        if(counter==0){
