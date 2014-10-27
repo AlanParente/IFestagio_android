893b3edab56bc66b9120a19b223a433d85bfb97b
Ruriel <ruriel.santos@gmail.com>
2014-10-27 17:34:11 -0300

    Adicionado animação de transição entre o logo da empresa e a descrição da vaga e adicionado cores que variam ao pressionar os botões aceitar, recusar e info.

--- src/com/example/estagio/OfertasActivity.java
+++ src/com/example/estagio/OfertasActivity.java
@@ -2,11 +2,19 @@ package com.example.estagio;
 
 
 import android.support.v7.app.ActionBarActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class OfertasActivity extends ActionBarActivity{
@@ -14,13 +22,20 @@ public class OfertasActivity extends ActionBarActivity{
 	private ImageButton check;
 	private ImageButton refuse;
 	private ImageButton info;
+	private ImageView logo;
 	private TextView infoText;
 	private TextView companyTxt;
+	private TextView desTxt;
 	private String ch = "";
 	private String salario = "";
 	private String empresa = "Nome da empresa";
+	private String descricao = "Aqui será inserido a descrição da"
+			+ " vaga ao apertar a tecla de informação localizada no meio"
+			+ " A descrição deve parar antes do nome da empresa. Posteriormente"
+			+ " contaremos os caracteres para se ter uma descrição mais precisa."
+			+ " Por isso estou enchendo esse texto de besteira para se ter uma boa"
+			+ " noção do tamanho máximo que a descrição poderia ter.";
 	private static int imgcolor;
-	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
@@ -32,20 +47,21 @@ public class OfertasActivity extends ActionBarActivity{
 		refuse = (ImageButton) findViewById(R.id.refuseButton);
 		info = (ImageButton) findViewById(R.id.infoButton);
 		infoText = (TextView) findViewById(R.id.infoTxt);
+		logo = (ImageView) findViewById(R.id.imgCompany);
 		companyTxt = (TextView) findViewById(R.id.companyTxt);
+		desTxt = (TextView) findViewById(R.id.descriptionTxt);
+		desTxt.setVisibility(View.INVISIBLE);
 		imgcolor = Color.rgb(102, 102, 102);
 		check.setColorFilter(imgcolor);
 		refuse.setColorFilter(imgcolor);
 		info.setColorFilter(imgcolor);
+		infoText.setText("Salário: "+salario+"\nCH: "+ch);
+		companyTxt.setText(empresa);
+		desTxt.setText(descricao);
 		
 	}
 	
-	@Override
-	public void onPostResume()
-	{
-		infoText.setText("Salário: "+salario+"\nCH: "+ch);
-		companyTxt.setText(empresa);
-	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -64,6 +80,41 @@ public class OfertasActivity extends ActionBarActivity{
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
 	
+		@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+		@SuppressLint("NewApi")
+		public void onInfoButtonClick(View v)
+		{
+			int duracao = getResources().getInteger(android.R.integer.config_shortAnimTime);
+			if(logo.getVisibility() == View.VISIBLE)
+			{
+				desTxt.setAlpha(0f);
+				desTxt.setVisibility(View.VISIBLE);
+				desTxt.animate().alpha(1f).setDuration(duracao).setListener(null);
+				
+				logo.animate().alpha(0f).setListener(new AnimatorListenerAdapter()
+				{
+					@Override
+					public void onAnimationEnd(Animator animation)
+					{
+						logo.setVisibility(View.GONE);
+					}
+				});
+			}
+			else
+			{
+				logo.setAlpha(0f);
+				logo.setVisibility(View.VISIBLE);
+				logo.animate().alpha(1f).setDuration(duracao).setListener(null);
+				
+				desTxt.animate().alpha(0f).setListener(new AnimatorListenerAdapter()
+				{
+					@Override
+					public void onAnimationEnd(Animator animation)
+					{
+						desTxt.setVisibility(View.GONE);
+					}
+				});
+			}
+		}
 }
